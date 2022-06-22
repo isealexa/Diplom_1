14469bc65056312d60b0797106277cb9e065af61
Ekaterina Smagina <i.sealexa@yandex.ru>
2022-06-22 11:17:29 +0300

    Add two tests in IngredientTypeTest for enum IngredientType

--- src/test/java/IngredientTypeTest.java
+++ src/test/java/IngredientTypeTest.java
@@ -2,8 +2,10 @@ import org.junit.Before;
 import org.junit.Test;
 import praktikum.IngredientType;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
 
 public class IngredientTypeTest {
 
@@ -28,5 +30,25 @@ public class IngredientTypeTest {
     @Test
     public void checkEnumIngredientTypeContainsFILLING(){
         assertEquals("FILLING", ingredientType[1].toString());
+    }
+
+    @Test
+    public void checkEnumIngredientTypeDoesNtContainBUN(){
+        String incorrectType = "BUN";
+        boolean expected = false;
+
+        String[] arrayStringTypes = Stream.of(IngredientType.values()).map(IngredientType::name).toArray(String[]::new);
+        boolean actual = Arrays.asList(arrayStringTypes).contains(incorrectType);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkEnumIngredientTypeDoesNtContainEmptyElement(){
+        String incorrectType = "";
+        boolean expected = false;
+
+        String[] arrayStringTypes = Stream.of(IngredientType.values()).map(IngredientType::name).toArray(String[]::new);
+        boolean actual = Arrays.asList(arrayStringTypes).contains(incorrectType);
+        assertEquals(expected, actual);
     }
 }
